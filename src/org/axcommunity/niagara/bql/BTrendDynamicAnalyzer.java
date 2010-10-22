65ad0d9b39282ac35918b854d0c8db4ffb23ebb8
arnottm <arnottm@users.sourceforge.net>
2010-10-22 12:54:56 +0000

    (no commit message)
    svn-revision: 80

---
+++ src/org/axcommunity/niagara/bql/BTrendDynamicAnalyzer.java
@@ -0,0 +1,186 @@
+package org.axcommunity.niagara.bql;
+
+import com.tridium.bql.util.BDynamicTimeRange;
+import javax.baja.collection.BITable;
+import javax.baja.collection.ColumnList;
+import javax.baja.collection.TableCursor;
+import javax.baja.naming.BOrd;
+import javax.baja.naming.OrdTarget;
+import javax.baja.status.BStatusNumeric;
+import javax.baja.sys.Action;
+import javax.baja.sys.BAbsTime;
+import javax.baja.sys.BComponent;
+import javax.baja.sys.BIcon;
+import javax.baja.sys.BMonth;
+import javax.baja.sys.BObject;
+import javax.baja.sys.Property;
+import javax.baja.sys.Sys;
+import javax.baja.sys.Type;
+import javax.baja.util.BAbsTimeRange;
+
+public class BTrendDynamicAnalyzer extends BComponent
+{
+  public static final Property history = newProperty(0, BOrd.NULL, null);
+  public static final Property dateRange = newProperty(0, new BAbsTimeRange(BAbsTime.make(2000, BMonth.make(0), 0, 0, 0), BAbsTime.make()), null);
+  public static final Property timeRange = newProperty(10, BDynamicTimeRange.make("lastMonth"), null);
+  public static final Property timeRangeDelta = newProperty(10, false, null);
+  public static final Property count = newProperty(8, new BStatusNumeric(0.0D), null);
+  public static final Property min = newProperty(8, new BStatusNumeric(0.0D), null);
+  public static final Property max = newProperty(8, new BStatusNumeric(0.0D), null);
+  public static final Property average = newProperty(8, new BStatusNumeric(0.0D), null);
+  public static final Property sum = newProperty(8, new BStatusNumeric(0.0D), null);
+  public static final Action execute = newAction(0, null);
+  public static final Type TYPE;
+  static Class class$org$axcommunity$niagara$bql$BTrendDynamicAnalyzer;
+
+  public BOrd getHistory()
+  {
+    return ((BOrd)get(history));
+  }
+
+  public void setHistory(BOrd paramBOrd)
+  {
+    set(history, paramBOrd, null);
+  }
+
+  public BAbsTimeRange getDateRange()
+  {
+    return ((BAbsTimeRange)get(dateRange));
+  }
+
+  public void setDateRange(BAbsTimeRange paramBAbsTimeRange)
+  {
+    set(dateRange, paramBAbsTimeRange, null);
+  }
+
+  public BDynamicTimeRange getTimeRange()
+  {
+    return ((BDynamicTimeRange)get(timeRange));
+  }
+
+  public void setTimeRange(BDynamicTimeRange paramBDynamicTimeRange)
+  {
+    set(timeRange, paramBDynamicTimeRange, null);
+  }
+
+  public boolean getTimeRangeDelta()
+  {
+    return getBoolean(timeRangeDelta);
+  }
+
+  public void setTimeRangeDelta(boolean paramBoolean)
+  {
+    setBoolean(timeRangeDelta, paramBoolean, null);
+  }
+
+  public BStatusNumeric getCount()
+  {
+    return ((BStatusNumeric)get(count));
+  }
+
+  public void setCount(BStatusNumeric paramBStatusNumeric)
+  {
+    set(count, paramBStatusNumeric, null);
+  }
+
+  public BStatusNumeric getMin()
+  {
+    return ((BStatusNumeric)get(min));
+  }
+
+  public void setMin(BStatusNumeric paramBStatusNumeric)
+  {
+    set(min, paramBStatusNumeric, null);
+  }
+
+  public BStatusNumeric getMax()
+  {
+    return ((BStatusNumeric)get(max));
+  }
+
+  public void setMax(BStatusNumeric paramBStatusNumeric)
+  {
+    set(max, paramBStatusNumeric, null);
+  }
+
+  public BStatusNumeric getAverage()
+  {
+    return ((BStatusNumeric)get(average));
+  }
+
+  public void setAverage(BStatusNumeric paramBStatusNumeric)
+  {
+    set(average, paramBStatusNumeric, null);
+  }
+
+  public BStatusNumeric getSum()
+  {
+    return ((BStatusNumeric)get(sum));
+  }
+
+  public void setSum(BStatusNumeric paramBStatusNumeric)
+  {
+    set(sum, paramBStatusNumeric, null);
+  }
+
+  public void execute()
+  {
+    invoke(execute, null, null);
+  }
+
+  public Type getType()
+  {
+    return TYPE;
+  }
+
+  public void doExecute()
+  {
+    String str1 = "1577846298735";
+
+    String str2 = getHistory().toString();
+
+    BOrd localBOrd = BOrd.make(str2 + "?period=" + getTimeRange() + ";delta=" + getTimeRangeDelta() + "|bql:historyFunc:HistoryRollup.rollup(select *, baja:RelTime '" + str1 + "')");
+
+    BITable localBITable = (BITable)localBOrd.resolve(Sys.getStation()).get();
+    ColumnList localColumnList = localBITable.getColumns();
+    TableCursor localTableCursor = (TableCursor)localBITable.cursor();
+
+    localTableCursor.next();
+
+    double d1 = Double.parseDouble(localTableCursor.get(localColumnList.get(2)).toString());
+    double d2 = Double.parseDouble(localTableCursor.get(localColumnList.get(3)).toString());
+    double d3 = Double.parseDouble(localTableCursor.get(localColumnList.get(4)).toString());
+    double d4 = Double.parseDouble(localTableCursor.get(localColumnList.get(5)).toString());
+    double d5 = Double.parseDouble(localTableCursor.get(localColumnList.get(6)).toString());
+
+    getCount().setValue(d1);
+    getMin().setValue(d2);
+    getMax().setValue(d3);
+    getAverage().setValue(d4);
+    getSum().setValue(d5);
+  }
+  public BIcon getIcon() { return icon; }
+  private static final BIcon icon = BIcon.make("local:|module://axCommunity/org/axcommunity/niagara/graphics/BAS_Logo.png");
+
+
+  static Class jdMethod_class(String paramString, boolean paramBoolean)
+  {
+    try
+    {
+      if (!(paramBoolean));
+      return Class.forName(paramString).getComponentType();
+    }
+    catch (ClassNotFoundException localClassNotFoundException)
+    {
+      throw new NoClassDefFoundError(localClassNotFoundException.getMessage());
+    }
+  }
+
+  static
+  {
+    Class tmp163_160 = class$org$axcommunity$niagara$bql$BTrendDynamicAnalyzer; 
+    if (tmp163_160 == null) tmp163_160=tmp163_160; 
+    TYPE = Sys.loadType(BTrendDynamicAnalyzer.class$org$axcommunity$niagara$bql$BTrendDynamicAnalyzer = jdMethod_class("[Lorg.axcommunity.niagara.bql.BTrendDynamicAnalyzer;", false));
+   
+  }
+}
---
+++ src/org/axcommunity/niagara/conversion/BEnumFromCommaSepVar.java
@@ -0,0 +1,85 @@
+package org.axcommunity.niagara.conversion;
+
+
+import java.util.StringTokenizer;
+/** Extension of the EnumWritable that accepts a csv string, one per line, as the range
+ * @author Mike Arnott, Kors Engineering
+*/
+
+import javax.baja.control.BEnumWritable;
+import javax.baja.log.Log;
+import javax.baja.naming.SlotPath;
+import javax.baja.status.BStatusString;
+import javax.baja.sys.BEnumRange;
+import javax.baja.sys.BFacets;
+import javax.baja.sys.BIcon;
+import javax.baja.sys.Context;
+import javax.baja.sys.Flags;
+import javax.baja.sys.Property;
+import javax.baja.sys.Sys;
+import javax.baja.sys.Type;
+import javax.baja.util.*;
+
+public class BEnumFromCommaSepVar extends BEnumWritable
+{
+  /** 
+   * This the csv list to use for the enum string outputs
+   * To use, put in a value something like:
+   * 1,Hello World
+   * 2,This is Line 2
+   * 3,Get the idea?
+   * */
+  public static final Property inCommaSepVar = newProperty(Flags.SUMMARY,new BStatusString(""),BFacets.make("multiLine",true));
+  public BStatusString getInCommaSepVar() {
+    return (BStatusString)get(inCommaSepVar);
+  }
+  public void setInCommaSepVar(BStatusString v) {
+    set(inCommaSepVar, v);
+  }
+  
+  public void started(){
+    this.setFacets(BFacets.make("multiLine",true));    
+  }
+  
+  
+  public void changed(Property property, Context context){
+    if(!Sys.atSteadyState() || !isRunning()){
+        return;
+      }
+        if (property == inCommaSepVar) {
+          //parse carriage return delimited records
+          StringTokenizer tokzer = new StringTokenizer(getInCommaSepVar().getValue(),"\r\n");
+          int tokCount =tokzer.countTokens();
+          if(tokCount>=0)
+          {
+            String[] tokVals = new String[tokzer.countTokens()];
+            int[] tokKeys = new int[tokzer.countTokens()];
+            for (int i = 0;i<tokCount;i++){
+              String tmp = tokzer.nextToken();
+              //parse each line for the commas
+              String[] row = TextUtil.split(tmp, ',');
+              tokKeys[i] = Integer.parseInt(row[0]);
+              tokVals[i] = SlotPath.escape(row[1]);
+            }
+            //create enum range facets
+            BEnumRange range = BEnumRange.make(tokKeys, tokVals);
+            BFacets bf = BFacets.makeEnum(range);
+            //add multiline back in to the facets
+            bf = BFacets.make(bf,BFacets.make("multiLine",true));
+
+            this.setFacets(bf);
+
+          }
+        }
+  }
+  
+
+  public BIcon getIcon() { return icon; }
+  private static final BIcon icon = BIcon.make("local:|module://axCommunity/org/axcommunity/niagara/graphics/korsLogo.png");
+  public static final Log logger = Log.getLog("axCommunity.BEnumFromCommaSepVar");
+
+  public static final Type TYPE = Sys.loadType(BEnumFromCommaSepVar.class);
+  public Type getType() { return TYPE; }
+
+  
+}
