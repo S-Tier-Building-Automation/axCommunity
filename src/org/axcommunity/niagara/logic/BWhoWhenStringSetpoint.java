c514486ec676475dc85d8910c10c76fdd5239cd2
jkoffler <jkoffler@users.sourceforge.net>
2013-01-28 20:39:43 +0000

    V13.01.02 January 28, 2013 - MODIFIED 'BWhoWhenBooleanSetpoint' to include new slots: 'SetTrue' (action), 'SetFalse' (action), 	'outLogString' (status string), 'Value' (topic), 'LogString' (topic) and   'absTimeFacets' (facet) 	and fixed logic so that facets are applied. - Justin Koffler - MODIFIED 'BWhoWhenStringSetpoint' to include new slots: 'outLogString' (status string), 'Value' (topic), 'LogString' (topic) and 'absTimeFacets' (facet) - Justin Koffler
    svn-revision: 138

--- src/org/axcommunity/niagara/logic/BWhoWhenStringSetpoint.java
+++ src/org/axcommunity/niagara/logic/BWhoWhenStringSetpoint.java
@@ -4,77 +4,108 @@ import javax.baja.sys.*;
 import javax.baja.status.*;
 
 /**
- * String Setpoint object that remembers the username/timestamp of the last change
- *
- * @author    Mike Arnott
- * @creation  9 Dec 11
- */
+* String Setpoint object that remembers the username/timestamp of the last change
+*
+* @author    Mike Arnott
+* @creation  9 Dec 11
+*/
 public class BWhoWhenStringSetpoint
 extends BComponent
 {
+	public static final Property absTimeFacets			= newProperty(0, BFacets.make(BFacets.SHOW_DATE,			BBoolean.make(true), 
+	                                          			                              BFacets.SHOW_TIME,			BBoolean.make(true), 
+	                                          			                              BFacets.SHOW_MILLISECONDS,	BBoolean.make(true)),null);
+	public BFacets getAbsTimeFacets(){ return (BFacets)get(absTimeFacets); }
+	public void setAbsTimeFacets(BFacets v){ set(absTimeFacets,v,null); }
+	
+	
+	public static String valueCurrent;
+	public static String  stringToLog;
+	
+	public static final Property facets = newProperty(0, BFacets.DEFAULT);
+	public BFacets getFacets() { return (BFacets)get(facets); }
+	public void setFacets(BFacets v) { set(facets, v); }
 
-  public static final Property facets = newProperty(0, BFacets.DEFAULT);
-  public BFacets getFacets() { return (BFacets)get(facets); }
-  public void setFacets(BFacets v) { set(facets, v); }
-  
-  /**The output setpoint*/
-  public static final Property out = newProperty(Flags.SUMMARY, new BStatusString());
-  public void setOut(BStatusString v) {     set(out, v);   }
-  public BStatusString getOut() {     return (BStatusString)get(out);   }
-
-  
-  /**String showing username of who changed the point*/
-  public static final Property changedBy = newProperty(Flags.READONLY + Flags.SUMMARY + Flags.DEFAULT_ON_CLONE, new BStatusString());
-  public void setChangedBy(BStatusString v) { set(changedBy, v); }
-  public BStatusString getChangedBy() { 
-    return (BStatusString)get(changedBy); 
-  }
-  
-  /**Absolute Time of change*/
-  public final static Property timeChanged = newProperty(Flags.SUMMARY + Flags.READONLY+ Flags.DEFAULT_ON_CLONE, BAbsTime.DEFAULT);
-  public void setTimeChanged(BAbsTime v) { set(timeChanged, v); }
-  public BAbsTime getTimeChanged() { return (BAbsTime)get(timeChanged); }
-
-  /**invokable action to set the current value*/
-  public static final Action SetValue = newAction(Flags.OPERATOR,BString.DEFAULT);
-  public void SetValue(BString v)
-  {
-    invoke(SetValue, null);
-  }
-  public void doSetValue(BString v, Context cxin)
-  {
-    if(cxin==null)
-    {
-      //for wiresheet invokes, set username to "logic"
-      getChangedBy().setValue("logic");
-    }
-
-    else 
-   {
-      //for any user invokes, get the context username
-      getChangedBy().setValue(cxin.getUser().getUsername());
-    }
-    //set value and timestamp
-    getOut().setValue(v.getString());
-    
-    //some day would love to add override status to this object also...
-    getOut().setStatus(BStatus.ok);
-    setTimeChanged(BAbsTime.make());
-  }
-  
-  public BIcon getIcon() { return icon; }
-  private static final BIcon icon = BIcon.make("local:|module://axCommunity/org/axcommunity/niagara/graphics/korsLogo.png");
-
-  
-  public Type getType() { return TYPE; }
-  public static final Type TYPE = Sys.loadType(BWhoWhenStringSetpoint.class);
+	/**The output setpoint*/
+	public static final Property out = newProperty(Flags.SUMMARY, new BStatusString());
+	public void setOut(BStatusString v) {     set(out, v);   }
+	public BStatusString getOut() {     return (BStatusString)get(out);   }
 
 
-  
-  public BValue getActionParameterDefault(Action action)
-  {
-    if (action == SetValue) 
-      return getOut().getValueValue();
-    return super.getActionParameterDefault(action);
-  }
+	/**String showing username of who changed the point*/
+	public static final Property changedBy = newProperty(Flags.READONLY + Flags.SUMMARY + Flags.DEFAULT_ON_CLONE, new BStatusString());
+	public void setChangedBy(BStatusString v) { set(changedBy, v); }
+	public BStatusString getChangedBy() { 
+	return (BStatusString)get(changedBy); 
+	}
+
+	/**Absolute Time of change*/
+	public final static Property timeChanged = newProperty(Flags.SUMMARY + Flags.READONLY+ Flags.DEFAULT_ON_CLONE, BAbsTime.DEFAULT);
+	public void setTimeChanged(BAbsTime v) { set(timeChanged, v); }
+	public BAbsTime getTimeChanged() { return (BAbsTime)get(timeChanged); }
+
+	/**
+	* String container information related to when,who,what and where something was change.
+	* The thought here was that several of these object could be linked to a single history.
+	*/
+	public static final Property outLogString = newProperty(0, new BStatusString());
+	public BStatusString getOutLogString() { return (BStatusString)get(outLogString);}
+	public void setOutLogString(BStatusString v) {set(outLogString,v);}
+	
+	
+	/**invokable action to set the current value*/
+	public static final Action SetValue = newAction(Flags.OPERATOR,BString.DEFAULT);
+	public void SetValue(BString v){invoke(SetValue, null);}
+	public void doSetValue(BString v, Context cxin)
+	{
+		valueCurrent = getOut().getValue();
+		
+		if(cxin==null)
+		{
+		//for wiresheet invokes, set username to "logic"
+		getChangedBy().setValue("logic");
+		}
+
+		else 
+		{
+		//for any user invokes, get the context username
+		getChangedBy().setValue(cxin.getUser().getUsername());
+		}
+		//set value and timestamp
+		getOut().setValue(v.getString());
+		fireValue(BString.make(v.getString()));
+
+		//some day would love to add override status to this object also...
+		getOut().setStatus(BStatus.ok);
+		setTimeChanged(BAbsTime.make());
+		
+		stringToLog	= getTimeChanged().toString(getAbsTimeFacets()) + "," + cxin.getUser().getUsername() + ",FROM: " + valueCurrent + ",TO: " + getOut().getValue() + ",SLOTPATH: " + getSlotPath();
+		getOutLogString().setValue(stringToLog);
+		fireLogString(BString.make(stringToLog));
+	}
+
+
+	public static final Topic Value = newTopic(0);
+	public void fireValue(BString event){fire(Value,event,null);}
+	
+	
+	public static final Topic LogString = newTopic(0);
+	public void fireLogString(BString event){fire(LogString,event,null);}
+
+
+	public BIcon getIcon() { return icon; }
+	private static final BIcon icon = BIcon.make("local:|module://axCommunity/org/axcommunity/niagara/graphics/korsLogo.png");
+
+
+	public Type getType() { return TYPE; }
+	public static final Type TYPE = Sys.loadType(BWhoWhenStringSetpoint.class);
+
+
+
+	public BValue getActionParameterDefault(Action action)
+	{
+		if (action == SetValue) 
+		return getOut().getValueValue();
+		return super.getActionParameterDefault(action);
+	}
 }
