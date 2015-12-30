178e40f09c9e05c242491000c006b7f8503044a5
jkoffler <jkoffler@users.sourceforge.net>
2015-12-30 17:33:35 +0000

    MODIFIED BEnumFromCommaSepVar - Added new slot 'inCsvWithNoIndex' in which the input values need only be comma seperated and the index values will be automativally created. No need for new line seperator.
    svn-revision: 164

--- src/org/axcommunity/niagara/conversion/BEnumFromCommaSepVar.java
+++ src/org/axcommunity/niagara/conversion/BEnumFromCommaSepVar.java
@@ -3,7 +3,7 @@ package org.axcommunity.niagara.conversion;
 
 import java.util.StringTokenizer;
 /** Extension of the EnumWritable that accepts a csv string, one per line, as the range
- * @author Mike Arnott, Kors Engineering
+* @author Mike Arnott, Kors Engineering
 */
 
 import javax.baja.control.BEnumWritable;
@@ -22,65 +22,105 @@ import javax.baja.util.*;
 
 public class BEnumFromCommaSepVar extends BEnumWritable
 {
-  /** 
-   * This the csv list to use for the enum string outputs
-   * To use, put in a value something like:
-   * 1,Hello World
-   * 2,This is Line 2
-   * 3,Get the idea?
-   * */
-  public static final Property inCommaSepVar = newProperty(Flags.SUMMARY,new BStatusString(""),BFacets.make("multiLine",true));
-  public BStatusString getInCommaSepVar() {
-    return (BStatusString)get(inCommaSepVar);
-  }
-  public void setInCommaSepVar(BStatusString v) {
-    set(inCommaSepVar, v);
-  }
-  
-  public void started(){
-    //setFacets(BFacets.make("multiLine",true);
-   
-  }
-  
-  
-  public void changed(Property property, Context context){
-    if(!Sys.atSteadyState() || !isRunning()){
-        return;
-      }
-        if (property == inCommaSepVar) {
-          //parse carriage return delimited records
-          StringTokenizer tokzer = new StringTokenizer(getInCommaSepVar().getValue(),"\r\n");
-          int tokCount =tokzer.countTokens();
-          if(tokCount>=0)
-          {
-            String[] tokVals = new String[tokzer.countTokens()];
-            int[] tokKeys = new int[tokzer.countTokens()];
-            for (int i = 0;i<tokCount;i++){
-              String tmp = tokzer.nextToken();
-              //parse each line for the commas
-              String[] row = TextUtil.split(tmp, ',');
-              tokKeys[i] = Integer.parseInt(row[0]);
-              tokVals[i] = SlotPath.escape(row[1]);
-            }
-            //create enum range facets
-            BEnumRange range = BEnumRange.make(tokKeys, tokVals);
-            BFacets bf = BFacets.makeEnum(range);
-            //add multiline back in to the facets
-            bf = BFacets.make(bf,BFacets.make("multiLine",true));
+	/**
+	* This the csv list to use for the enum string outputs
+	* To use, put in a value something like:
+	* 1,Hello World
+	* 2,This is Line 2
+	* 3,Get the idea?
+	* */
+	public static final Property inCommaSepVar = newProperty(Flags.SUMMARY,new BStatusString(""),BFacets.make("multiLine",true));
+	public BStatusString getInCommaSepVar() {return (BStatusString)get(inCommaSepVar);}
+	public void setInCommaSepVar(BStatusString v) {set(inCommaSepVar, v);}
 
-            this.setFacets(bf);
+	public static final Property inCsvWithNoIndex = newProperty(0,new BStatusString(""),BFacets.make("multiLine",true));
+	public BStatusString getInCsvWithNoIndex() {return (BStatusString)get(inCsvWithNoIndex);}
+	public void setInCsvWithNoIndex(BStatusString v) {set(inCsvWithNoIndex, v);}
+	
+	
+	public void started()
+	{
+		//setFacets(BFacets.make("multiLine",true);
+	}
 
-          }
-        }
-  }
-  
 
-  public BIcon getIcon() { return icon; }
-  private static final BIcon icon = BIcon.make("local:|module://axCommunity/org/axcommunity/niagara/graphics/korsLogo.png");
-  public static final Log logger = Log.getLog("axCommunity.BEnumFromCommaSepVar");
+	public void changed(Property property, Context context)
+	{
+		if(!Sys.atSteadyState() || !isRunning()){return;}
+		
+		if (property == inCommaSepVar)
+		{
+			//PARSE CARRIAGE RETURN DELIMITED RECORDS
+			StringTokenizer	tokzer		= new StringTokenizer(getInCommaSepVar().getValue(),"\r\n");
+			int				tokCount	= tokzer.countTokens();
+			
+			if(tokCount>=0)
+			{
+				String[]	tokVals	= new String[tokzer.countTokens()];
+				int[]		tokKeys	= new int[tokzer.countTokens()];
+				
+				for (int i = 0;i<tokCount;i++)
+				{
+					String		tmp	= tokzer.nextToken();
+					
+					//PARSE EACH LINE FOR THE COMMAS
+					String[]	row	= TextUtil.split(tmp, ',');
+					tokKeys[i]		= Integer.parseInt(row[0]);
+					tokVals[i]		= SlotPath.escape(row[1]);
+				}
+				
+				//CREATE ENUM RANGE FACETS
+				BEnumRange	range	= BEnumRange.make(tokKeys, tokVals);
+				BFacets		bf		= BFacets.makeEnum(range);
+				
+				//ADD MULTILINE BACK IN TO THE FACETS
+				bf = BFacets.make(bf,BFacets.make("multiLine",true));
 
-  public static final Type TYPE = Sys.loadType(BEnumFromCommaSepVar.class);
-  public Type getType() { return TYPE; }
+				this.setFacets(bf);
+			}
+		}
+		
+		
+		if (property == inCsvWithNoIndex)
+		{
+			//PARSE COMMA DELIMITED RECORDS
+			StringTokenizer	tokzer		= new StringTokenizer(getInCsvWithNoIndex().getValue(),",");
+			int				tokCount	= tokzer.countTokens();
+			
+			if(tokCount>=0)
+			{
+				String[]	tokVals	= new String[tokzer.countTokens()];
+				int[]		tokKeys	= new int[tokzer.countTokens()];
+				
+				for (int i = 0;i<tokCount;i++)
+				{
+					String		tmp	= tokzer.nextToken();
+					
+					tokKeys[i]		= i;
+					tokVals[i]		= tmp;
+				}
+				
+				//CREATE ENUM RANGE FACETS
+				BEnumRange	range	= BEnumRange.make(tokKeys, tokVals);
+				BFacets		bf		= BFacets.makeEnum(range);
+				
+				//ADD MULTILINE BACK IN TO THE FACETS
+				bf = BFacets.make(bf,BFacets.make("multiLine",true));
 
-  
+				this.setFacets(bf);
+			}
+			
+			
+		}
+	}
+
+
+	public BIcon getIcon() { return icon; }
+	private static final BIcon icon = BIcon.make("local:|module://axCommunity/org/axcommunity/niagara/graphics/korsLogo.png");
+	public static final Log logger = Log.getLog("axCommunity.BEnumFromCommaSepVar");
+
+	public static final Type TYPE = Sys.loadType(BEnumFromCommaSepVar.class);
+	public Type getType() { return TYPE; }
+
+
 }
