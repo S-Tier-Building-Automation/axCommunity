1bc1a763691d6ab22affbb517be006b0ecbef00e
jkoffler <jkoffler@users.sourceforge.net>
2018-11-06 18:51:17 +0000

    - MODIFIED 'BSetNumericAction' * Added new action slot 'SetNumericValue' that will set the value directly. * Added new numeric slot 'outLastValueSet' which represents the last value set.
    svn-revision: 234

--- N4/axCommunity-rt/src/org/axcommunity/niagara/logic/BSetNumericAction.java
+++ N4/axCommunity-rt/src/org/axcommunity/niagara/logic/BSetNumericAction.java
@@ -1,5 +1,6 @@
 package org.axcommunity.niagara.logic;
 
+import javax.baja.status.BStatus;
 import javax.baja.status.BStatusBoolean;
 import javax.baja.status.BStatusNumeric;
 import javax.baja.sys.*;
@@ -9,15 +10,16 @@ import javax.baja.sys.*;
  * To use, link the New Numeric Input output to the "set" action on any numeric writable.
  * @author Mike Arnott, Kors Engineering
 */
-public class BSetNumericAction extends BComponent{
+public class BSetNumericAction extends BComponent
+{
 	private boolean lastBoolean = false;
 
 	
-    public void changed(Property property, Context context){
+    public void changed(Property property, Context context)
+    {
         super.changed(property, context);
-       	if(!Sys.atSteadyState()|| !isRunning()){
-    		return;
-    	}
+       	if(!Sys.atSteadyState()|| !isRunning()){return;}
+       	
       	if(property == trigger)
        	{
        		if(getTrigger().getValue()==true&&lastBoolean==false)
@@ -25,8 +27,8 @@ public class BSetNumericAction extends BComponent{
        			//new value, fire output
        			this.fireNewNumericInput(BDouble.make(getInNumber().getValue()));
        		}
+       		
    			lastBoolean = getTrigger().getValue();
-
        	}
 
     }
@@ -34,33 +36,64 @@ public class BSetNumericAction extends BComponent{
     
 	/**invokable action to send the current value*/
     public static final Action SetValue = newAction(0);
-	public void SetValue()
-	{
-		invoke(SetValue, null);
-	}
+	public void SetValue(){invoke(SetValue, null);}
 	public void doSetValue()
 	{
 		this.fireNewNumericInput(BDouble.make(getInNumber().getValue()));	
 	}
+	
+	/**invokable action to set the current value directly. If no links exist on 'inNumber' slot then it will be updated also.*/
+	public static final Action SetNumericValue = newAction(Flags.DEFAULT_ON_CLONE, (BValue)BDouble.TYPE.getInstance(), BFacets.makeNumeric(0));
+	public void SetNumericValue(BDouble v){ invoke(SetNumericValue, v, null); }
+	public void doSetNumericValue(BDouble v, Context cx)
+	{
+		if( this.getLinks(this.getSlot("inNumber")).length <= 0 )
+		{
+			getInNumber().setValue(v.getDouble());
+			getOutLastValueSet().setValue(v.getDouble());
+			this.fireNewNumericInput(BDouble.make( getInNumber().getValue() ));
+		}
+		else
+		{
+			getOutLastValueSet().setValue(v.getDouble());
+			this.fireNewNumericInput(BDouble.make( v.getDouble() ));
+		}
+	}
+	
+	
+	/** Gets Action parameter default value */
+	public BValue getActionParameterDefault(Action paramAction)
+	{
+		if (paramAction == SetNumericValue)
+		{
+			return (BValue) BDouble.make(getOutLastValueSet().getValue());
+		}
+		
+		return super.getActionParameterDefault(paramAction);
+	}
+	
 
     /**Numeric value to be sent*/
     public static final Property inNumber = newProperty(Flags.SUMMARY, new BStatusNumeric());
     public BStatusNumeric getInNumber() { return (BStatusNumeric)get(inNumber);}
     public void setInNumber(BStatusNumeric v) {set(inNumber,v);}
 
-	
     /**Boolean Input, false to true transition invokes the send*/
     public static final Property trigger = newProperty(Flags.SUMMARY, new BStatusBoolean());
     public BStatusBoolean getTrigger() { return (BStatusBoolean)get(trigger);}
     public void setTrigger(BStatusBoolean v) {set(trigger,v);}
 
+    /**Numeric Output representing the last value set.*/
+	public static final Property outLastValueSet = newProperty(Flags.DEFAULT_ON_CLONE, new BStatusNumeric(0, BStatus.ok), BFacets.make(BFacets.FIELD_WIDTH, BInteger.make(50)));
+	public BStatusNumeric getOutLastValueSet() { return (BStatusNumeric) get(outLastValueSet); }
+	public void setOutLastValueSet(BStatusNumeric v) { set(outLastValueSet, v); }
+	
     /**Event fired to send new Number*/    
     public static final Topic newNumericInput = newTopic(0);
-    public void fireNewNumericInput(BDouble event){
-    	fire(newNumericInput,event,null);
-    }
+    public void fireNewNumericInput(BDouble event){fire(newNumericInput,event,null); }
+    
     public BIcon getIcon() { return icon; }
-    private static final BIcon icon = BIcon.make("module://axCommunity/org/axcommunity/niagara/graphics/korsLogo.png");
+    private static final BIcon icon = BIcon.make("local:|module://axCommunity/org/axcommunity/niagara/graphics/korsLogo.png");
 	    
 	public static final Type TYPE = Sys.loadType(BSetNumericAction.class);
 	public Type getType() { return TYPE; }
